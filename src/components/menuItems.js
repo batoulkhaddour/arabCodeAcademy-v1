724d46d7671541be7c032d76dd6cab00a084f716
batoulkhaddour <batoulkhaddour783@gmail.com>
2024-05-23 12:54:56 +0300

    Add files via upload

--- src/components/menuItems.js
+++ src/components/menuItems.js
@@ -1,10 +1,12 @@
 import {
     Flex, Link, Button, Box, IconButton, Image, Menu, MenuButton, MenuList, MenuItem, useBreakpointValue
 } from "@chakra-ui/react"
-import { HamburgerIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, ChevronDownIcon, CloseIcon } from '@chakra-ui/icons';
+import { useState } from "react";
 
 function MenuItems() {
     const isMobile = useBreakpointValue({ base: true, md: false });
+    const [display, setdisplay] = useState('none')
     return (
         <Box
             flexBasis={{ base: "100%", md: "20%", sm: "20%" }}
@@ -59,7 +61,8 @@ function MenuItems() {
                             التواصل
                         </Button>
                     </Link>
-                    <Link href="/" passHref>
+
+                    <Box>
                         <Menu>
                             <MenuButton as={Button} colorScheme='none' color="white" variant='ghost' rightIcon={<ChevronDownIcon />}>
                                 المصادر
@@ -71,7 +74,8 @@ function MenuItems() {
                                 <MenuItem> 2</MenuItem>
                             </MenuList>
                         </Menu>
-                    </Link>
+                    </Box>
+
 
 
                     <Link href="/" passHref>
@@ -97,8 +101,113 @@ function MenuItems() {
                     aria-label="Open menu"
                     icon={<HamburgerIcon />}
                     colorScheme="whiteAlpha"
+                    onClick={() => { setdisplay('flex') }}
                 />
             )}
+
+            <Flex
+                w="100vw"
+                bg="brand.primary"
+                zIndex={100}
+                h='100vh'
+                pos='fixed'
+                top={0}
+                left={0}
+                direction={{ base: "column", md: "column" }}
+                display={display}
+            >
+                <Flex justify='flex-end'>
+                    <IconButton
+                        m={10}
+                        aria-label="close menu"
+                        size='lg'
+                        colorScheme="whiteAlpha"
+                        icon={
+                            <CloseIcon />
+                        }
+                        onClick={() => { setdisplay('none') }}
+                    />
+                </Flex>
+                <Flex
+                    align="center"
+                    direction={{ base: "column", md: "column" }}
+                >
+                    <Button my={2} colorScheme='red' w={175}>
+                        تسجيل الدخول
+                        <Image src="/images/icons/iconLogin.png" ml={2} maxW='20px' />
+                    </Button>
+                    <Button my={2} colorScheme='blue' w={175}>
+                        انشاء حساب
+                        <Image src="/images/icons/🦆 icon _profile circled_.png" ml={2} maxW='20px' />
+                    </Button>
+
+                    <Link href="/" passHref>
+
+                        <Button
+                            colorScheme='none'
+                            color="white"
+                            variant='ghost'
+                            aria-label='Educational-paths'
+                            my={2}
+                            w='100%'
+                        >
+
+                            المسارات التعليمية
+                        </Button>
+
+                    </Link>
+
+                    <Link href="/#" passHref>
+
+                        <Button
+                            colorScheme='none'
+                            color="white"
+                            variant='ghost'
+                            aria-label='contact'
+                            my={2}
+                            w='100%'
+                        >
+                            التواصل
+                        </Button>
+
+                    </Link>
+
+                    <Box>
+
+                        <Menu>
+
+                            <MenuButton as={Button} colorScheme='none' color="white" variant='ghost' rightIcon={<ChevronDownIcon />}>
+                                المصادر
+                            </MenuButton>
+                            <MenuList
+                                color='#000'
+                                my={2}
+                            >
+                                <MenuItem> 1</MenuItem>
+                                <MenuItem> 2</MenuItem>
+                            </MenuList>
+
+                        </Menu>
+
+                    </Box>
+
+                    <Link href="/" passHref>
+
+                        <Button
+                            colorScheme='none'
+                            color="white"
+                            variant='ghost'
+                            aria-label='Educational-paths'
+                            my={5}
+                            w='90%'
+                        >
+                            <Image src='/images/icons/Frame 13.png' maxW='80px' />
+                        </Button>
+
+                    </Link>
+
+                </Flex>
+            </Flex>
         </Box>
 
     )
--- src/components/sliderItem.js
+++ src/components/sliderItem.js
@@ -7,32 +7,39 @@ function SliderItem(props) {
             position="relative"
             align="center"
             justify="space-between"
-            flexDirection={{ base: "row", md: "row", sm: "column" }}
+            flexDirection={{ base: "row", md: "row", sm: "column-reverse" }}
             wrap="wrap"
             px="3rem"
+            overflowX='hidden'
             bgGradient="linear(to-r, #88559B, #0E125F)"
             color="white">
             <Image src='/images/arrow.png' alt="arrow"
                 position="absolute"
-                left={0}
-                top={{ base: "60px", md: "60px", sm: "160px" }}
+                left={{ base: "-20px", md: "-20px", sm: "-160px" }}
+                top={{ base: "75px", md: "85px", sm: "190px" }}
             />
             <Box>
                 <Text color="white"
-                    pl={{ base: "300px", md: "300px", sm: "200px" }}
-                    w={600}
+                    fontSize={{ base: "28", md: "28", sm: "22" }}
+                    ml={{ base: "200", md: "200", sm: "100" }}
+                    mt={{ base: "20", md: "20", sm: "-280px" }}
+                    textAlign='right'
+                    maxW={{ base: "400px", md: "400px", sm: "300px" }}
 
                 >{props.text}</Text>
                 <Button colorScheme="teal"
                     position="absolute"
-                    left={20}
+                    left={{ base: "20", md: "20", sm: "120" }}
                     bottom={20}
-                    w={200}
+                    w={250}
+                    fontSize={{ base: "22", md: "22", sm: "20" }}
+                    py={8}
                 > المسارات التعليمية
                     <Image src="/images/icons/🦆 icon _Chalkboard Teacher_.png" ml={2} maxW='20px' /></Button>
             </Box>
             <Box>
-                <Image src={props.image} alt="slide image" objectFit="cover" />
+                <Image src={props.image} alt="slide image" objectFit="cover" maxW={{ base: "500px", md: "500px", sm: "250px" }}
+                 />
             </Box>
         </Flex>
     )
